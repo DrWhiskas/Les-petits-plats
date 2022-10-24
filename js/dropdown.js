2ce05f11c9068043d65023ab3eca25aba918b44b
unknown <kayv@hotmail.fr>
2022-10-24 12:27:43 +0200

    .

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,2 +1,8 @@
+import * as recipes from "./data/recipes"
+
 const oui = document.getElementById("ingredients");
 const non = document.getElementById("ingredients-item");
+
+recipes.forEach((element) => {
+    console.log("oui");
+});
