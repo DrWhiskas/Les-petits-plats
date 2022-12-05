69bee1b9a5f6c81b708aca730939de602c1198e1
unknown <kayv@hotmail.fr>
2022-12-05 11:56:04 +0100

    Update filter.js

--- js/utils/filter.js
+++ js/utils/filter.js
@@ -3,34 +3,61 @@ let arrayIngredients = []
 let arrayAppliance = [];
 let arrayUstensils = [];
 
+let checkIngredient = false
+let checkUstensil = false
+let checkApplicances = false
+
 /* DOM */
 
 const ingredientSelector = document.getElementById('ingredients');
 const appareilsSelector = document.getElementById('appareils');
 const ustensilesSelector = document.getElementById('ustensiles');
 
+const test = document.getElementById('gazo')
 const buttonSelector = document.querySelectorAll('.filter__items__input');
 
 
-ustensilesSelector.addEventListener('click', (e) => {
-	getUstensiles(recipes)
+appareilsSelector.addEventListener('click', (e) => {
+	e.preventDefault();
+	checkApplicances = true
+	toto(arrayAppliance);
+	
 });
 
-/* usetensils */
-function getUstensiles(data) {
-	const listselector = document.getElementById('sadge')
+ingredientSelector.addEventListener('click', (e) => {
+	e.preventDefault();
+	checkIngredient = true;
+	toto(arrayIngredients);
+	console.log(arrayIngredients);
 	
-	data.forEach((recipe) => {
-		arrayUstensils.push(recipe.ustensils);
-		const newList = document.createElement('li');
-		newList.classList.add('oui');
-		newList.innerHTML = `
-		<li>${recipe.ustensils}</li>
-		`;
-		document.body.appendChild(newList);
-	});
-}
+});
+ustensilesSelector.addEventListener('click', (e) => {
+	e.preventDefault();
+	checkUstensil = true
+	toto(arrayUstensils);
+	console.log(arrayUstensils);
+});
 
-function displaydata(data){
-    
-}   
+/* GET DATA */
+
+function toto (testArray){
+	recipes.forEach((recipe) => {
+		if (checkIngredient == true) {
+			recipe.ingredients.forEach((ingredient) => {
+				testArray.push(ingredient.ingredient);
+			})
+			checkIngredient = false;
+		} 
+		if (checkUstensil == true) {
+			recipe.ustensils.forEach((ustensil) =>{
+			testArray.push(ustensil);
+			})
+			checkUstensil = false
+		}
+		if (checkApplicances = true){
+			testArray.push(recipe.appliance);
+		}
+		checkApplicances = false
+	});
+	
+}
