6b26f31c56cb8d542f6bb68176e320b279797a32
unknown <kayv@hotmail.fr>
2023-01-16 13:07:32 +0100

    filter done

--- js/utils/newFilter.js
+++ js/utils/newFilter.js
@@ -1,4 +1,55 @@
+function setList(){
+    newArray.forEach((item) => {
+        item.ustensils.forEach((use) =>{
+            usetensilArray.push(use)
+        })
+        appArray.push(item.appliance);
+	});
+    /*item.ingredients.forEach((ingredient) =>{
+        ingredientArray.push(ingredient)
+    })*/
+    usetensilArray = [...new Set(usetensilArray)];
+    appArray = [...new Set(appArray)];
+
+    console.log(usetensilArray , 'use');
+    console.log(appArray , 'app');
+    console.log(ingredientArray);
+
+}
+    setList();
 /* SELECTOR */
+
+/* USETENSILES */ 
 const usentensilsSelector = document.getElementById('ustensiles');
 
-console.log(newArray, "oui");
+usentensilsSelector.addEventListener('click', (e)=>{
+
+    const boxFilter = document.getElementById('filter__box__usetensils');
+    const usentensilList = document.getElementById(
+			'filter__box__usetensils__list'
+		);
+    usentensilsSelector.classList.toggle('active')
+    boxFilter.classList.toggle('hidden')
+    console.log('oui');
+    //displayList(usentensilList, usetensilArray);
+
+})
+
+/* APPARREILS */
+const appSelector = document.getElementById('appareils');
+
+    appSelector.addEventListener('click', (e) => {
+	const boxFilter = document.getElementById('filter__box__appareils');
+	const appilList = document.getElementById(
+		'filter__box__appareils__list'
+	);
+	appSelector.classList.toggle('active');
+	boxFilter.classList.toggle('hidden');
+	console.log(testArray);
+});
+
+function displayList(target, array) {
+	array.forEach((item) => {
+		target.innerHTML += `<li class="filter__item">${item}</li>`;
+	});
+}
--- js/utils/searchBar.js
+++ js/utils/searchBar.js
@@ -1,4 +1,8 @@
 let newArray = [];
+let usetensilArray = []
+let ingredientArray = [];
+let appArray = []
+let testArray = []
 // DOM
 const searchSelection = document.getElementById('search');
 const recipesSection = document.getElementById('recipes');
@@ -15,17 +19,25 @@ searchSelection.addEventListener('keyup', (e) => {
 function searchRecipe(keys) {
 	recipesSection.innerHTML = '';
 	newArray = []
+	usetensilArray = []
+	appArray =[]
+	testArray = []
 	if (keys.length > 2) {
 		recipes.forEach((recipe) => {
 			/* RECIPES TITLES */
 			if (recipe.name.toLowerCase().includes(keys)) {
 				newArray.push(recipe);
+				//toto();
 			}
 			/* USTENSILS */
 			recipe.ustensils.forEach((ustensil) => {
 				if (ustensil.toLowerCase().includes(keys)) {
 					newArray.push(recipe);
+					usetensilArray.push(recipe.ustensils)
+					
+
 				}
+				
 				/* APPAREILS */
 				if (recipe.appliance.toLowerCase().includes(keys)) {
 					newArray.push(recipe);
@@ -35,13 +47,21 @@ function searchRecipe(keys) {
 			recipe.ingredients.forEach((newingredient)=>{
 				if(newingredient.ingredient.toLowerCase().includes(keys)){
 					newArray.push(recipe);
+					//toto();
 				}
 			})
-
+		
 		})
+		setList();
 		newArray = [...new Set(newArray)];
 		recipesDisplay(newArray);
 	} else {
 		recipesDisplay(recipes);
 	}
+}
+
+function oui(tableau){
+	tableau.forEach((sadge) =>{
+		console.log(sadge);
+	});
 }
