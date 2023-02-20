42b00dc52970e9f2e8d7220da23c3096fcd1bb47
unknown <kayv@hotmail.fr>
2023-02-20 13:55:28 +0100

    tri

--- js/utils/tag.js
+++ js/utils/tag.js
@@ -1,13 +1,14 @@
 /*  CREATION DES TAG */
+let tagArray = [];
 function createTag(){
     const tagSelector = document.getElementById('tag');
-    const tagItems = document.querySelectorAll('.filter__item')
+    let tagItems = document.querySelectorAll('.filter__item')
 
     const usentensilList = document.getElementById('filter__box__usetensils__list');	
 	const appList = document.getElementById('filter__box__appareils__list');
 	const ingredientList = document.getElementById('filter__box__ingredient__list');
 
-    let tagArray = []
+    
     tagItems.forEach((item) => {
     item.addEventListener('click', (e) => {
        let text = e.target.textContent;
@@ -29,67 +30,88 @@ function createTag(){
             tag.style.backgroundColor = '#3282F7';
             tag.setAttribute('type', 'ingredient')
         }
-        item.style.display = 'none';
+        
         tagArray.push(`${type}${text}`);
         checkIngredient == false
 		tag.innerHTML = `${text} <i class="fa-regular fa-circle-xmark"></i>`;
     
         // filtre du tag
         filterTag(tagArray);
+        e.target.style.display = 'none';
+        console.log(usentensilList);
+
+        
+
         tagSelector.appendChild(tag)  
         /* CLIQUE POUR SUPPRIMER UN TAG ET LE REMETTRE DANS LA LISTE */
         tag.addEventListener('click', (e) => {
-					let tagSelected = e.target;
-					/*if (tagSelected.hasAttribute('type','ingredient')) {	
-                    }*/
 					removeTag(tag, tagArray, newArray);
-				});
+		    });
+
+            tagItems = document.querySelectorAll('.filter__item');
+						tagItems.forEach((newItem) => {
+							if (newItem.innerText == e.target.innerText) {
+								newItem.style.display = 'none';
+							}
+						});
         })
         /* REMETTRE A ZERO SI LA LISTE DE TAG EST VIDE */
         
     })
+
 }
 /* RETIRER UN TAG */
 function removeTag(tag, liste, toto) {
-	tag.style.display = 'none'
-     
+	tag.remove()
+
     filterTag(liste);
     resetRecipes(); 
     recipesDisplay(newArray)
     setList(toto)
-    console.log(toto);
-    console.log(liste);
+
 }
 /* FILTRE LES TAG */
 function filterTag(tags) {
 	let newTag = [];
     let tagSelected = []
+    console.log(tags);
         newArray.forEach((recipe) => {
+            let checkRecipe = true 
 					tags.forEach((tag) => {
+                        let checkTag = false
 						if (tag.includes('appareil_')) {
 							let tagSplit = tag.split('_')[1];
 							if (tagSplit.includes(recipe.appliance)) {
-								newTag.push(recipe);
-								tagSelected.push({ tagSplit, type: 'appareil' });
+								checkTag = true
+								//tagSelected.push({ tagSplit, type: 'appareil' });
 							}
 						} else if (tag.includes('usentensil_')) {
 							let tagSplit = tag.split('_')[1];
 							recipe.ustensils.forEach((usentesil) => {
 								if (tagSplit.includes(usentesil)) {
-									newTag.push(recipe);
-									tagSelected.push({ tagSplit, type: 'usetensil' });
+									checkTag = true;
+									//tagSelected.push({ tagSplit, type: 'usetensil' });
 								}
 							});
 						} else {
 							let tagSplit = tag.split('_')[1];
 							recipe.ingredients.forEach((ingredient) => {
 								if (tagSplit.includes(ingredient.ingredient)) {
-									newTag.push(recipe);
-									tagSelected.push({ tagSplit, type: 'ingredient' });
+									checkTag = true;
+									//tagSelected.push({ tagSplit, type: 'ingredient' });
 								}
 							});
 						}
+                        console.log(recipe);
+                        console.log(tag);
+                        console.log(checkTag);
+                        if(checkTag == false){
+                        checkRecipe = false
+                    }
 					});
+                    if(checkRecipe == true){
+                    newTag.push(recipe)
+                }
 				});
                 
                 
